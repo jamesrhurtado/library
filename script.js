9cf35ac3eb77a6e29e31d6e85af0cb1304481fbf
jrenzzo <u202118568@upc.edu.pe>
2022-02-16 10:08:09 -0500

    added files

---
+++ script.js
@@ -0,0 +1,22 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.page = page
+    this.read = read
+    this.info = function(){
+        if(read){
+            return  "The "+ title+ "by "+author+", "+pages+", has been read"
+        }else{	
+            return  "The "+ title+ "by "+author+", "+pages+", not read yet"
+        }
+    }   
+}
+
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+
